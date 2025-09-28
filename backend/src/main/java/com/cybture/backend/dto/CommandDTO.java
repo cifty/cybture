43b6830a4619c0adf50917cd18a1b9b5021c2033
cifty <vcifty@gmail.com>
2025-09-28 21:42:38 +0300

    test

--- backend/src/main/java/com/cybture/backend/dto/CommandDTO.java
+++ backend/src/main/java/com/cybture/backend/dto/CommandDTO.java
@@ -1,4 +1,10 @@
 package com.cybture.backend.dto;
 
+import lombok.Data;
+
+@Data
 public class CommandDTO {
+    private String description;
+    private String tool;
+    private String category;
 }
